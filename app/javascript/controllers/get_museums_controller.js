7804ef683b7b24abfb28839d0b6d3ed9878ffd5b
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-12-08 21:19:15 -0500

    created rails app and implemented mapbox api to get list of museums near a set of latitude and longitude

---
+++ app/javascript/controllers/get_museums_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="get-museums"
+export default class extends Controller {
+  static targets = ['museums']
+  connect() {
+    // console.log('hello');
+  }
+
+  getMuseums() {
+    const lng = 13.437641
+    const lat = 52.494857
+
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat},museums.json?access_token=pk.eyJ1IjoiamlhY292b3p6aSIsImEiOiJjbGExYTU3bTQwNWhrM3FvM3NvdDA5ZndmIn0.t699gs3HWXZ0cLccVxz8-A`)
+      .then((response) => response.json())
+      .then((data) => {
+        // data.features.forEach(museum)
+          console.log(data);
+        // TRYING TO LOOP THROUGH EVERY KEY-VALUE PAIR
+        //   data.features.forEach(obj => {
+        //     Object.entries(obj).forEach(([key, value]) => {
+        //         console.log(`${key} ${value}`);
+        //     });
+        //     console.log('-------------------');
+        // });
+
+          // TRYING TO GROUP EVERY MUSEUM RESULT(FEATURE) BY POSTCODE
+          // const grouped = _.groupBy(data.features, 'context[0]');
+          // console.log(grouped);
+
+          // INSERTING POSTCODE OF FIRST MUSEUM
+          // const museum = `{
+          //   "${data.features[0].context[0].text}": ["${data.features[0].text}"]
+          // }`
+          // this.museumsTarget.innerHTML = museum
+
+  });
+  }
+}
